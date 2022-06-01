0461cc839e5b2a60d7c3925b751f3a7c1a2531f0
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-01 15:16:34 -0700

    added psuedo code to direct js construction

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,79 @@
+//questions div
+//list items of possible answers
+//endgame div
+//timer
 
+
+//gen variables
+//score
+//time
+//timer
+//questions left (referencing indexes) var questionIndex = 0
+//feedback variable
+
+
+
+// event listener for start button
+// reference for button
+// add event listener and function
+
+// on start button click startGame function
+//start game function
+//hide intro start screen
+
+//render first question to the screen, show multiple choice answers
+//call funtion 
+
+
+//timer starts timer = setInterval(function(),1000)
+//time display
+
+//rend question function
+// change hidden value of questions div
+// variable with an array of object
+// var questions = [
+//     {
+//         title: "JS question",
+//         answerChoices: [1,2,3,4],
+//         correctAnswer: "answer"
+//     }
+// ]
+
+
+//reference index of question to display on screen
+//increment question index variable
+//grab html reference to header to attach question title to question[0].title append
+//grab html reference push question answers to an array loop through answers create list el or button and append to ul
+// add eventlistener to the answer choices/div that holds answer choices event.target matches the correct answer
+//function checkAnswer ()
+// variable selectedAnswer = event.target
+// if (selectedAnswer === question[0].correctAnswer)
+
+        // provide correct feedback
+        //Dom add in element 
+
+// else {
+//     //provide incorrect feedback
+//     //penalize time global time var and subtract time-= time taken off
+// }
+
+// function startTimer 
+// time --;
+// re render time on screen
+
+//warning close to zero
+
+// check timer if statement
+// if time === 0, clearInterval then end quiz else keep running this function
+
+//end quiz function
+//hide question and display intial forms
+//display final score
+//eventListener intials and score to local storage setItem("key",value)
+//getItem JSON.parse
+//stores score in an array and stringify to put in local storage pass the most rcent to array and store again
+
+// Highscores page
+//pull scores from local storage div with ol make references to those in ol
+//order high scores from highest to lowest
+// need buttons for html for go back and clear local storage

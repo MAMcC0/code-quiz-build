b29623fcc61372f90ed34276de925b0c57c3c62e
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-02 18:39:05 -0700

    added question object array variable

--- assets/logic.js
+++ assets/logic.js
@@ -6,19 +6,53 @@ var timerDisplay =  document.getElementsByClassName("count-timer");
 var feedback =  document.getElementsByClassName("feedback");
 var score =  document.getElementsByClassName("score");
 var landingPage = document.getElementsByClassName("landing-main");
-
-//gen variables
+var userInput = document.getElementsByClassName("user-input");
 var score = 0;
-//score
-//time
-//timer
 var questionsLeft = questions[i];
+
+
+var questionsArray = [
+   {
+        question1: "Which of the following is a primative data type in Javascript",
+        possibleAns1: ["sentence", "truthy", "int","boolean"],
+        correctAns1: "D"
+    },
+    {
+        question2: "What are the two types of scope JavaScript uses",
+        possibleAns2: ["Global and Local", "Surrounding and Inner", "Abroad and Local","Outside and Inside"],
+        correctAns2: "A"
+    },
+    {
+        question3: "We create a new branch off of our main branch with  'git branch test-branch'. How do we switch to our newly created branch?",
+        possibleAns3: ["git change test branch", "git commit test-branch", "git merge test-branch","git checkout test-branch"],
+        correctAns3: "D"
+    },
+    {
+        question4: "What value would we add to setInterval() if we want a function called, myTimer() to run every 3 seconds?",
+        possibleAns4: ["setInterval(myTimer, 3)", "setInterval(myTimer, 300)", "setInterval(myTimer, 3000)","setInterval(myTimer, 30)"],
+        correctAns4: "C"
+    },
+    {
+        question5: " You just finished the feature that you've been working on a successfully merged your branch, feature-52. How would you delete branch, feature-52?",
+        possibleAns5: ["git branch feature-52", "git merge feature-52", "git branch -d feature-52","git checkout feature-52"],
+        correctAns5: "C"
+    },
+    {
+        question6: "Which property can you use in order to implement event delegation?",
+        possibleAns: ["event.addEventListener()", "event.target", "event.stopPropagation()","event.preventDefault()"],
+        correctAns: "B"
+    }
+];
+
+
+
+
 
 
 
 // on start button click startGame function
 function startQuiz(){
-    if($('.title-section') === visible)
+    if() 
     score
 }
 //start game function
